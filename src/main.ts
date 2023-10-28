fb5a93cb81eaf429cc8f30998e86f11b3d477203
hanab11 <sxakln2020@163.com>
2023-10-29 00:18:57 +0800

    chore: 安装element-plus，并全局声明

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入element-plus插件、样式、国际化
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore 忽略当前文件ts类型的检测，否则有红色提示（打包会失败）
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import './style.css' // 本地默认样式 后期删除
 
-// 提交前格式化
-const a = 1
-function A(a) {
-  console.log('这是a', a)
-}
-A(a)
+// 创建应用实例对象
+const app = createApp(App)
+// 安装插件
+app.use(ElementPlus, { locale: zhCn })
+// 挂载
+app.mount('#app')
