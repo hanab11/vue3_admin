67deef6156e58ccbb91c47ecf5a80ba2c4c8759c
hanab11 <sxakln2020@163.com>
2023-11-18 02:19:58 +0800

    add: 菜单静态页面（注意容器实际高度 = height + padding * 2）

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -4,8 +4,29 @@
     <div class="layout_menu">
       <!-- logo组件 -->
       <logo></logo>
-      <!-- 展示菜单 -->
+      <!-- 菜单展示 -->
       <!-- 滚动组件 -->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件，其中每一项是el-menu-item -->
+        <el-menu background-color="black" text-color="white">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">导航1</el-menu-item>
+          <!-- 折叠菜单 -->
+          <el-sub-menu index="3">
+            <!-- 折叠菜单标题，必须用具名插槽title，插槽v-slot简写为# -->
+            <template #title><span>导航2(2级菜单)</span></template>
+            <el-menu-item index="3-1">AA</el-menu-item>
+            <el-menu-item index="3-2">BB</el-menu-item>
+            <el-menu-item index="3-3">CC</el-menu-item>
+            <el-sub-menu index="3-4">
+              <template #title><span>3级菜单</span></template>
+              <el-menu-item index="3-4-1">XXX</el-menu-item>
+              <el-menu-item index="3-4-2">YYY</el-menu-item>
+              <el-menu-item index="3-4-3">ZZZ</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航栏 -->
     <div class="layout_tabbar">456</div>
@@ -29,6 +50,12 @@ import logo from './logo/index.vue'
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      // 实际高度 = logo高度 + 内边距 * 2
+      height: calc(100vh - $base-logo-height - $base-logo-padding * 2);
+    }
   }
 
   .layout_tabbar {
@@ -42,11 +69,11 @@ import logo from './logo/index.vue'
 
   .layout_main {
     position: absolute; // absolute绝对定位
-    width: calc(100% - $base-menu-width);
-    height: calc(100vh - $base-tabbar-height);
+    width: calc(100% - $base-menu-width - $base-main-padding * 2);
+    height: calc(100vh - $base-tabbar-height - $base-main-padding * 2);
     left: $base-menu-width;
     top: $base-tabbar-height;
-    padding: 20px; // 内边距
+    padding: $base-main-padding; // 设置内边距，需要在宽高里减掉padding*2，否则会撑出滑动条
     overflow: auto; // 若太高自动延伸
   }
 }
--- src/layout/logo/index.vue
+++ src/layout/logo/index.vue
@@ -12,10 +12,10 @@ import setting from '@/setting'
 <style scoped lang="scss">
 .logo_container {
   width: 100%;
-  height: $base-menu-logo-height; // 容器高度
+  height: $base-logo-height; // 容器高度（不包括内边距）
   display: flex; // 弹性容器
   align-items: center; // 垂直居中对齐，以容器高度为准（text-align水平居中对齐）
-  padding: 20px;
+  padding: $base-logo-padding;
 
   img {
     width: 40px;
@@ -23,7 +23,7 @@ import setting from '@/setting'
   }
 
   p {
-    font-size: $base-menu-logo-title-fontSize;
+    font-size: $base-logo-title-fontSize;
     margin-left: 10px; // 元素自身外边距（left需配定位使用）
   }
 }
