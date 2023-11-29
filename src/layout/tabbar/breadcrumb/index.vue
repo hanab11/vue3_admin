5638069e9fd536f5a94f47b9203bbf9f1909b990
hanab11 <sxakln2020@163.com>
2023-11-30 01:07:50 +0800

    add: 顶部面包屑动态展示

--- src/layout/tabbar/breadcrumb/index.vue
+++ src/layout/tabbar/breadcrumb/index.vue
@@ -2,14 +2,34 @@
   <el-icon style="margin-right: 10px">
     <Expand />
   </el-icon>
-  <!-- el面包屑组件，其中每一项是el-breadcrumb-item，separator-icon图标分隔符 -->
+  <!-- el面包屑组件，其中每一项是el-breadcrumb-item，separator-icon图标分隔符，to属性让父路由可点击路由跳转 -->
   <el-breadcrumb separator-icon="ArrowRight">
-    <el-breadcrumb-item>权限管理</el-breadcrumb-item>
-    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-    <el-breadcrumb-item>Test</el-breadcrumb-item>
+    <!-- 遍历匹配数据 -->
+    <el-breadcrumb-item
+      v-for="(item, index) in $route.matched"
+      :key="index"
+      v-show="!item.meta.hidden"
+      :to="item.path"
+    >
+      <!-- 展示匹配的图标 -->
+      <el-icon class="icon_container">
+        <component :is="item.meta.icon"></component>
+      </el-icon>
+      <!-- 展示匹配的标题 -->
+      <span>{{ item.meta.title }}</span>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
 
-<style scoped></style>
+// 获取路由实例对象，$route.matched根据路由表得到匹配数据
+let $route = useRoute()
+</script>
+
+<style scoped lang="scss">
+.icon_container {
+  vertical-align: top; // 把元素的顶部与行中最高元素的顶部对齐（垂直对齐）
+}
+</style>
