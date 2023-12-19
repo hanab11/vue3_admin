0a09af6233fd5406edc32a110188df6509a99e48
hanab11 <sxakln2020@163.com>
2023-12-19 23:55:11 +0800

    add: 用真实接口替换掉mock接口

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,27 +1,22 @@
 // API统一管理，用户相关的接口
 import request from '@/utils/request'
 // 只引入类型
-import type {
-  loginFormData,
-  loginResponseData,
-  userInfoReponseData
-} from './type'
 
 // 枚举用户相关的请求地址
 enum API {
-  LOGIN_URL = '/user/login',
-  USERINFO_URL = '/user/info'
-  //LOGOUT_URL = '/user/logout'
+  LOGIN_URL = '/admin/acl/index/login',
+  USERINFO_URL = '/admin/acl/index/info',
+  LOGOUT_URL = '/admin/acl/index/logout'
 }
 
-// 暴露xxx请求函数（接口方法）（ts类型，在传入参数后加: ，在方法后加 <第一个是参数类型, 第二个是返回值类型>）
-// 登录请求
-export const reqLogin = (data: loginFormData) =>
-  request.post<any, loginResponseData>(API.LOGIN_URL, data) // 这里是接收一个请求参数any类型和响应数据loginResponseData类型
+// 暴露xxx请求函数（接口方法）（ts类型，在传入参数后加：，在方法后加 <第一个是参数类型, 第二个是返回值类型>）
 
-// 用户信息请求
-export const reqUserInfo = () =>
-  request.get<any, userInfoReponseData>(API.USERINFO_URL)
+// 登录请求接口
+export const reqLogin = (data: any) =>
+  request.post<any, any>(API.LOGIN_URL, data)
 
-// 退出登录请求
-//export const reqLogout = () => request.post<any, any>(API.LOGOUT_URL)
+// 用户信息请求接口
+export const reqUserInfo = () => request.get<any, any>(API.USERINFO_URL)
+
+// 退出登录请求接口
+export const reqLogout = () => request.post<any, any>(API.LOGOUT_URL)
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,9 +1,9 @@
 // 用户相关的小仓库模块
 import { defineStore } from 'pinia'
 // 引入接口：请求登录、请求用户信息
-import { reqLogin, reqUserInfo } from '@/api/user'
+import { reqLogin, reqLogout, reqUserInfo } from '@/api/user'
 // 引入ts数据类型
-import type { loginFormData } from '@/api/user/type'
+//import type { loginFormData } from '@/api/user/type'
 import type { UserState } from './types/type'
 // 引入本地存储数据的工具方法
 import { GET_TOKEN, SET_TOKEN, REMOVE_TOKEN } from '@/utils/token'
@@ -25,45 +25,54 @@ const useUserStore = defineStore('User', {
   actions: {
     // 用户登录方法，reqLogin需要传参，userLogin需要接收参数
     // 传入参数data报红，显示默认any类型，就需要给data标注数据类型（data: xxx）
-    async userLogin(data: loginFormData) {
+    async userLogin(data: any) {
       const result = await reqLogin(data) // 异步请求会返回promise对象，使用async await，注result自动推断ts类型
+      //console.log(result) // 接口响应result:{code, message, data, ok} data成功是token，失败是提示信息
 
       // 登录成功 200 -> 获得 token，并存储到state
       // 登录失败 201 -> 获得 失败信息
       if (result.code === 200) {
-        this.token = result.data.token as string
+        this.token = result.data as string
         // 由于pinia/vuex并非持久化，所以必须本地存储一份持久化token（cookie也可以做）
-        SET_TOKEN(result.data.token as string)
+        SET_TOKEN(result.data as string)
 
         // 保证当前async函数，返回成功的promise，暂时写死
         return 'OK'
       } else {
         // 返回失败的promise，用reject方法，传入参数最好是error实例对象（等价error = new Error()）
-        return Promise.reject(new Error(result.data.message))
+        return Promise.reject(new Error(result.data))
       }
     },
 
     // 用户信息方法，获取用户信息需要带着token请求，在请求头里封装
     async userInfo() {
       const result = await reqUserInfo() // 异步请求，等待响应结果，注result自动推断ts类型
+      //console.log(result) // 接口响应result:{code, message, data:{name, avatar}, ok}
 
       // 判断成功，获得用户信息存储到state
       if (result.code === 200) {
-        this.username = result.data.userInfo.username
-        this.avatar = result.data.userInfo.avatar
+        this.username = result.data.name
+        this.avatar = result.data.avatar
         return 'ok'
       } else {
-        return Promise.reject('获取用户信息失败')
+        return Promise.reject(new Error(result.message))
       }
     },
 
     // 用户登出方法
-    userLogout() {
-      // 目前没有mock接口：退出登录接口（通知服务器本地用户唯一标识失效）
-      this.token = ''
-      //this.username = ''
-      //this.avatar = ''
-      REMOVE_TOKEN() // 清除本地存储的token
+    async userLogout() {
+      // 退出登录接口（通知服务器本地用户唯一标识失效）
+      const result = await reqLogout()
+
+      if (result.code == 200) {
+        this.token = ''
+        this.username = ''
+        this.avatar = ''
+        REMOVE_TOKEN() // 清除本地存储的token
+        return 'ok'
+      } else {
+        return Promise.reject(new Error(result.message))
+      }
     }
   },
   // 计算属性
