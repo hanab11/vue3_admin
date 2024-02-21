cec8167ef16c336fd1df7c45d2be9aa89466d4cb
hanab11 <sxakln2020@163.com>
2024-02-21 23:15:30 +0800

    add: trademark组件，展示数据业务V2

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -44,11 +44,56 @@
       />
     </el-card>
 
-    <!-- 对话框组件，在添加品牌与修改已有品牌的业务时候使用结构 -->
-    <!-- 
-      v-model 属性用户控制对话框的显示与隐藏 true显示 false隐藏
+    <!-- 对话框组件，在添加品牌与修改已有品牌的业务时候使用结构 
+      v-model 属性用户控制对话框的显示与隐藏 true/false
       title 设置对话框左上角标题
     -->
+    <el-dialog
+      v-model="dialogFormVisible"
+      :title="trademarkParams.id ? '修改品牌' : '添加品牌'"
+    >
+      <el-form
+        style="width: 80%"
+        :model="trademarkParams"
+        :rules="rules"
+        ref="formRef"
+      >
+        <el-form-item label="品牌名称" label-width="100px" prop="tmName">
+          <el-input
+            placeholder="请您输入品牌名称"
+            v-model="trademarkParams.tmName"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px" prop="logoUrl">
+          <!-- upload组件属性:action图片上传路径书写/api,代理服务器不发送这次post请求  -->
+          <el-upload
+            class="avatar-uploader"
+            action="/api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img
+              v-if="trademarkParams.logoUrl"
+              :src="trademarkParams.logoUrl"
+              class="avatar"
+            />
+            <el-icon v-else class="avatar-uploader-icon">
+              <Plus />
+            </el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <!-- 具名插槽footer -->
+      <template #footer>
+        <el-button type="primary" size="default" @click="cancel">
+          取消
+        </el-button>
+        <el-button type="primary" size="default" @click="confirm">
+          确定
+        </el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
